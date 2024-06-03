41a1cf4c90083aff1723fb5460404218c9539990
Ricardo Ferreira <66833693+f-ricardo@users.noreply.github.com>
2024-06-03 19:41:28 +0000

    First commit test message for someone.

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+function Home() {
+    return (
+    <div>
+        <h1>Hola Emi!</h1>
+        <p> Esto forma parte de un curso/proyecto que estoy desarrollando, y así como dejar el dólar fue una oportunidad para decirte algunas cosas, ¡aquí tengo otra! jejejeje </p>
+        <p> Estaba mirando el calendario ahora, y ya hace 1 mes que estuve allí y los recuerdos todavía están muy vivos en mi memoria y parece que fue ayer. ¡Otra oportunidad para decir gracias por todos esos días increíbles! </p>
+        <p> Como dije la otra vez, me encantaría repetir, ya sea en Argentina o en Brasil. ¿Vamos? </p>
+        <p>¡Y ya van casi 3 meses que nos hablamos todos los días! Me siento muy a gusto y feliz con nuestras conversaciones. </p>
+        <p>Como sabes, no soy muy bueno con las palabras, pero el sentimiento es de agradecimiento y alegría por haberte conocido y haber vivido esto contigo hasta ahora.</p>
+        <p>Te deseo lo mejor hoy y siempre.</p>        
+        <p>Un enorme beso, Rick</p>        
+    </div>
+);
+   
+    
+}
+
+export default Home;
