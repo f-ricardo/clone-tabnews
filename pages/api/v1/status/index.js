cca02ba0cdb074a1ac252e5b1da9bfdcdf01ac14
Ricardo Ferreira <66833693+f-ricardo@users.noreply.github.com>
2024-07-04 14:14:34 +0000

    add config docker `.env.development` `compose.yaml` and connection DB

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(resquet, response) {
+import database from "../../../../infra/database.js";
+
+async function status(resquet, response) {
+  const result = await database.query("SELECT 1 + 1 as soma;");
+  console.log(result.rows);
+
   response
     .status(200)
     .json({ chave: "os alunos do curso .dev são acima da média" });
--- tests/api/v1/get.test.js
+++ tests/api/v1/get.test.js
@@ -1,4 +1,5 @@
 test("api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+  console.log(response.body);
   expect(response.status).toBe(200);
 });
