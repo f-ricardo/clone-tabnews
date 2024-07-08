97d318e7ebe73abbcd6db30814535e4bfef0d65c
Ricardo Ferreira <66833693+f-ricardo@users.noreply.github.com>
2024-07-08 13:57:46 +0000

    finalizando endpoint status

--- tests/api/v1/get.test.js
+++ tests/api/v1/get.test.js
@@ -1,5 +1,15 @@
 test("api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-  console.log(response.body);
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.update_at).toBeDefined();
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpdateAt);
+
+  expect(responseBody.dependencies.database.version).toBe("16.3");
+
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
 });
